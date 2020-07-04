556838abe17f46df2334f70c0c83778ac0909cf0
Patricia <prmonday@gmail.com>
2020-07-04 18:32:31 +0100

    .DS_Store

---
+++ Adding_step6.rb
@@ -0,0 +1,19 @@
+students = [
+["Dr. Hannibal Lecter", :november],
+["Darth Vader", :november],
+["Nurse Ratche", :november],
+["Michael Corleone", :november],
+["Alex DeLarge", :noember],
+["The Wiched Witch of the West",:november],
+["Terminator", :november],
+["Freddy Kruegfer" , :november],
+["The Joker",:november],
+["Joffrey Baratheon", :november],
+["Norman Bates", :november]
+
+]
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
+  end
+end 
