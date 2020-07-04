c3f1f035130673d764f4dd024731a9743064cb02
Patricia <prmonday@gmail.com>
2020-07-04 14:27:45 +0100

    directory.rb

--- directory.rb
+++ directory.rb
@@ -11,3 +11,30 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+
+student_count = 11
+#first we print the list of students
+puts "The students of Villains Academy"
+puts "_____________"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+# finalyy, we print the total number of students
+
+print "Overall, we have "
+#it's important that print() doesn't add new line characters
+print 11
+puts "great students"
+print student_count
+puts "great students"
+
+# finally, we print the total number of students
