290a500e9be4b8099eb33fa37bd7910779aa7e1a
Patricia <prmonday@gmail.com>
2020-07-04 14:46:39 +0100

    directory.rb

--- directory.rb
+++ directory.rb
@@ -1,18 +1,3 @@
-puts "The students of Villains Academy"
-puts "_____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-
-
 student_count = 11
 #first we print the list of students
 puts "The students of Villains Academy"
@@ -28,13 +13,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-# finalyy, we print the total number of students
-
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-puts "great students"
-print student_count
-puts "great students"
-
 # finally, we print the total number of students
+puts "Overall, we have #{student_count} great students" 
